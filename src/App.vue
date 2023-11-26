9fe3c2cf878d6f35318dc6aeb04e7853d2dd296e
DrakoPOD <Emmanuel1093@hotmail.com>
2023-11-26 15:43:02 -0400

    finish tests of the component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,79 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container container-fluid p-3">
+    <div class="row g-2">
+      <div class="col-12 d-flex flex-row">
+        <h1 class="flex-grow-1">Task Board</h1>
+        <button class="btn btn-primary" @click="openModal = true">
+          Add Task
+        </button>
+      </div>
+      <div class="col-12">
+        <div class="row">
+          <Column
+            v-for="{ key, name } in columsNames"
+            :key="key"
+            :title="name"
+            :loading="isLoading"
+            :task-list="taskByStatus[key]"
+          />
+        </div>
+        <Modal v-model="openModal" persistent>
+          <div class="card">
+            <div class="card-body" style="width: 400px">
+              <NewTask mode="add" />
+              <button class="btn btn-danger mt-2" @click="openModal = false">
+                Close
+              </button>
+            </div>
+          </div>
+        </Modal>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+import { computed, onMounted, ref } from "vue";
+import Column from "./components/Column.vue";
+import NewTask from "./components/NewTask.vue";
+import Modal from "@/components/Modal.vue";
+
+import { TaskStatusNames, TaskStatus } from "@/types/task.enums";
+import type { ITask, ITaskStutus } from "./types/task";
+
+import { useTaskStore } from "@/store/taskStore";
+
+const openModal = ref(false);
+const isLoading = ref(true);
+
+const taskStore = useTaskStore();
+const taskList = ref<ITask[]>([]);
+
+const columsNames = Object.keys(TaskStatusNames).map((key) => ({
+  key: Number(key),
+  name: TaskStatusNames[Number(key) as ITaskStutus],
+}));
+
+async function getTaskList() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  taskList.value = taskStore.tasks;
+  isLoading.value = false;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
+const taskFiltered = computed(() => taskList.value);
+
+const taskByStatus = computed(() => {
+  const cols: Record<number, ITask[]> = {};
+  columsNames.forEach((status) => {
+    cols[status.key] = taskFiltered.value.filter((x) => x.status == status.key);
+  });
+  return cols;
+});
+
+onMounted(async () => {
+  await getTaskList();
+});
+</script>
+
+<style scoped></style>
